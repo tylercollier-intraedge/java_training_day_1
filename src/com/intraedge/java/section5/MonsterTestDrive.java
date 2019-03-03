d0ff9d13ff29ea72f74c57d2d9485dc6f6d79e97
EC2 Default User <ec2-user@ip-172-31-19-152.us-west-2.compute.internal>
2019-03-03 14:05:32 +0000

    section 5

---
+++ src/com/intraedge/java/section5/MonsterTestDrive.java
@@ -0,0 +1,26 @@
+public class MonsterTestDrive {
+	public static void main(String[] args) {
+    	Monster[] ma = new Monster[3];
+    	ma[0] = new Vampire();
+    	ma[1] = new Dragon();
+    	ma[2] = new Monster();
+    	for(int x=0; x<3; x++) {
+    		ma[x].frighten(x);
+    	}
+	}
+}
+
+class Monster {
+	// A
+}
+
+class Vampire extends Monster {
+	// B
+}
+
+class Dragon extends Monster {
+	boolean frighten(int degree) {
+		System.out.println("breathe fire");
+		return true;
+	}
+}
